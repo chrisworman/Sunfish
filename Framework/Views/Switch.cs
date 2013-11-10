a9122439ff66f837b7f0ecc7e8bb8490172aa25e
chrisworman <chrisworman@gmail.com>
2013-11-10 15:24:37 -0600

    Added a tap delegate to the factory constructors

--- Framework/Views/Switch.cs
+++ Framework/Views/Switch.cs
@@ -18,25 +18,31 @@ namespace Sunfish.Views {
 
 		public OnToggleDelegate OnToggle;
 
-		public static Switch CreateOn (Texture2D onTexture, Texture2D offTexture, Constants.ViewLayer layer) {
-			return new Switch (onTexture, onTexture, offTexture, true, new Vector2 (0, 0), layer);
+		public Views.View.OnTapDelegate SwitchOnTapDelegate;
+
+		public static Switch CreateOn (Texture2D onTexture, Texture2D offTexture, Constants.ViewLayer layer, View.OnTapDelegate switchOnTapDelegate) {
+			return new Switch (onTexture, onTexture, offTexture, true, new Vector2 (0, 0), layer, switchOnTapDelegate);
 		}
 
-		public static Switch CreateOff (Texture2D onTexture, Texture2D offTexture, Constants.ViewLayer layer) {
-			return new Switch (offTexture, onTexture, offTexture, false, new Vector2 (0, 0), layer);
+		public static Switch CreateOff (Texture2D onTexture, Texture2D offTexture, Constants.ViewLayer layer, View.OnTapDelegate switchOnTapDelegate) {
+			return new Switch (offTexture, onTexture, offTexture, false, new Vector2 (0, 0), layer, switchOnTapDelegate);
 		}
 
-		protected Switch (Texture2D currentTexture, Texture2D onTexture, Texture2D offTexture, bool isOn, Vector2 texturePosition, Constants.ViewLayer layer) :
+		protected Switch (Texture2D currentTexture, Texture2D onTexture, Texture2D offTexture, bool isOn, Vector2 texturePosition, Constants.ViewLayer layer, View.OnTapDelegate switchOnTapDelegate) :
 		base(currentTexture, texturePosition, layer) {
 			OnTexture = onTexture;
 			OffTexture = offTexture;
 			IsOn = isOn;
 			EnableTapGesture (HandleTap);
+			SwitchOnTapDelegate = switchOnTapDelegate;
 		}
 
 		private void HandleTap(View thisSwitch)
 		{
 			Toggle (true);
+			if (SwitchOnTapDelegate != null) {
+				SwitchOnTapDelegate (this);
+			}
 		}
 
 		public bool Toggle(bool callOnToggle = true) {
