66153cf1ace961dcbdf4c6a75fbdf80fbac89efd
unknown-user <unknown-user@172.16.1.69>
2013-11-10 10:33:08 -0600

    Added class comments

--- Framework/Views/Sprite.cs
+++ Framework/Views/Sprite.cs
@@ -5,6 +5,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Input.Touch;
 
+// A view based upon a texture
 namespace Sunfish.Views
 {
 
