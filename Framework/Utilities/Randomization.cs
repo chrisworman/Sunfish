771514e191521fc17c0aa3a4b777d8e627f9f707
chrisworman <chrisworman@gmail.com>
2013-12-07 19:07:55 -0600

    Added more utility randomization functions and organized code.

--- Framework/Utilities/Randomization.cs
+++ Framework/Utilities/Randomization.cs
@@ -8,15 +8,16 @@ namespace Sunfish.Utilities
 
 		public static Random RandomGenerator = new Random ();
 
+		#region "Bool"
+
 		public static bool NextBool()
 		{
-			return NextDouble() > 0.5d;
+			return NextDouble() >= 0.5d;
 		}
 
-		public static double NextDouble()
-		{
-			return RandomGenerator.NextDouble ();
-		}
+		#endregion
+
+		#region "Float"
 
 		public static float NextFloat()
 		{
@@ -25,20 +26,59 @@ namespace Sunfish.Utilities
 
 		public static float NextFloat(float min, float max)
 		{
-			float magnitude = Math.Abs (max - min);
-			float randomMagnitude = (float) NextDouble() * magnitude;
-			return min + randomMagnitude;
-		}
-
-		public static Vector2 NextVector2(Vector2 min, Vector2 max)
-		{
-			return new Vector2 (NextFloat (min.X, max.X), NextFloat (min.Y, max.Y));
+			return (float)NextDouble ((double)min, (double)max);
 		}
 
 		public static float NextFloatSign()
 		{
 			return NextBool () ? -1.0f : 1f;
 		}
+
+		#endregion
+
+		#region "Double"
+
+		public static double NextDouble()
+		{
+			return RandomGenerator.NextDouble ();
+		}
+
+		public static double NextDouble(double min, double max)
+		{
+			double magnitude = Math.Abs (max - min);
+			double randomMagnitude = NextDouble() * magnitude;
+			return min + randomMagnitude;
+		}
+
+		#endregion
+
+		#region "Vector2"
+
+		public static Vector2 NextVector2(Vector2 min, Vector2 max)
+		{
+			return new Vector2 (NextFloat (min.X, max.X), NextFloat (min.Y, max.Y));
+		}
+
+		public static Vector2 NextVector2(Vector2 max)
+		{
+			return new Vector2 (NextFloat (0f, max.X), NextFloat (0f, max.Y));
+		}
+
+		public static Vector2 NextVector2Sign()
+		{
+			return new Vector2 (NextFloatSign (), NextFloatSign ());
+		}
+
+		public static Vector2 NextVector2Perturbed(Vector2 vectorToPerturb, Vector2 maxPerturbation)
+		{
+			Vector2 pertubation = NextVector2 (maxPerturbation);
+			pertubation = Vector2.Multiply (pertubation, NextVector2Sign ());
+
+			return Vector2.Add (vectorToPerturb, pertubation);
+
+		}
+
+		#endregion
 
 	}
 }
